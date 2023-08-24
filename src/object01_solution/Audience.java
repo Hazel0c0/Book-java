8889d92014809444b3943b35616323410b89af13
Hazel0c0 <secretwish@naver.com>
2023-08-24 17:38:44 +0900

    ch01. 티켓판매 애플리케이션 구현하기 - 문제점 해결

---
+++ src/object01_solution/Audience.java
@@ -0,0 +1,25 @@
+package object01_solution;
+
+public class Audience {
+    // 관람객 : 소지품을 보관할 가방을 소유
+
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    // getBag() 제거
+
+    public Long buy(Ticket ticket) {
+        if (bag.hasInvitaion()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            // 티켓을 받으면 돈을 지불한다
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
+}
