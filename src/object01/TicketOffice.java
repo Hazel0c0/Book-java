8c0cb71bf784bed46d1f7034fa25a7cbdd33613f
Hazel0c0 <secretwish@naver.com>
2023-08-24 17:15:38 +0900

    ch01. 티켓판매 애플리케이션 구현하기

---
+++ src/object01/TicketOffice.java
@@ -0,0 +1,30 @@
+package object01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TicketOffice {
+    // 매표소
+
+    private Long amount;
+    private List<Ticket> tickets = new ArrayList<>();
+
+    public TicketOffice(Long amount, Ticket... tickets){
+        this.amount = amount;
+        this.tickets.addAll(Arrays.asList(tickets));
+    }
+    // 티켓 판매
+    public Ticket getTicket() {
+        return tickets.remove(0);
+    }
+
+
+    // 판매
+    public void minusAmount(Long amount){
+        this.amount -= amount;
+    }
+    public void plusAmount(Long amount){
+        this.amount += amount;
+    }
+}
