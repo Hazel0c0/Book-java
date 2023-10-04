513964e86bb096fe8b2096970fa6424e40fb714b
Hazel0c0 <secretwish5@naver.com>
2023-10-05 04:17:54 +0900

    ch02. 객체지향 프로그래밍

--- src/object01/Invitation.java
+++
@@ -1,9 +0,0 @@
-package object01;
-
-import java.time.LocalDateTime;
-
-public class Invitation {
-    // 초대장
-    private LocalDateTime when; // 초대일자
-
-}
--- src/object01/object01_solution/Invitation.java
+++
@@ -1,9 +0,0 @@
-package object01.object01_solution;
-
-import java.time.LocalDateTime;
-
-public class Invitation {
-    // 초대장
-    private LocalDateTime when; // 초대일자
-
-}
